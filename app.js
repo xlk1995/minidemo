4865c47c21c38a1b2d7da8af82106a675b27edf4
xlk <1148038404@qq.com>
2024-06-02 22:21:21 +0800

    feat: 强制更新小程序

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // app.js
-App({})
+App({
+
+  onLaunch(){
+    const updateManager = wx.getUpdateManager()
+    // 监听下载状态， 下载完之后触发onUpdateReady方法
+    updateManager.onUpdateReady(()=> {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        complete: (res) => {      
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+  }
+})
